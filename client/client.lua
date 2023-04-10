89b8573f04bdf7c2fb76a89611c1778d0ba27103
Füsti <118769783+FUst1ke@users.noreply.github.com>
2023-04-10 21:23:33 +0200

    updated anim with taskplayanim and attach system
    not the best rotation in the world but if you really need it rotate it :)

--- client/client.lua
+++ client/client.lua
@@ -1,10 +1,18 @@
 local function openMenu()
     local player = PlayerPedId()
-    TaskStartScenarioInPlace(player, "WORLD_HUMAN_CLIPBOARD", 0, true);
+    local dict = 'amb@code_human_wander_clipboard@male@base' 
+    local name = 'static'
+    local boneIndex = GetPedBoneIndex(player, 0x49D9)
+    local item = CreateObject(`prop_fib_clipboard`, GetEntityCoords(player), true, true, false)
+    AttachEntityToEntity(item, player, boneIndex, 0.02, 0.03, 0.05, -175.0, -100.0, 30.0, false, true, true, false, 0, true)
+    lib.requestAnimDict(dict, false)
+    TaskPlayAnim(player, dict, name, 8.0, 8.0, -1, 32, 0, true, true, true)
     local input = lib.inputDialog('Record a new incident', {'Title', 'Description'})
-    if not input then return end
+    if not input then DeleteEntity(item) ClearPedTasksImmediately(player) return end
     TriggerServerEvent('fusti_incident:sendData', input[1], input[2])
     ClearPedTasksImmediately(player)
+    DetachEntity(item, true, true)
+    DeleteEntity(item)
 end
 
 exports('openMenu', function()
